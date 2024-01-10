0d6cda80b6017778ea48145b31cda0ce661f5728
kakuritsu95 <ThodChristof@gmail.com>
2024-01-10 18:45:08 +0200

    command integ

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -3,8 +3,9 @@ import SearchOrder from '../features/order/SearchOrder';
 import Username from '../features/user/Username';
 
 function Header() {
+  const a = 5;
   return (
-    <header className="items-center flex justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
